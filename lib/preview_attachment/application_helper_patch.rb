674729a281455a6ad81ee9d630361052afb78189
Takenori Takaki <takenory@gmail.com>
2022-02-28 11:31:57 +0900

    Add test and refactor helper method

--- lib/preview_attachment/application_helper_patch.rb
+++ lib/preview_attachment/application_helper_patch.rb
@@ -10,30 +10,27 @@ module PreviewAttachment
 
     module InstanceMethods
       def link_to_attachment(attachment, options={})
-        previewable = Setting.enabled_redmica_ui_extension_feature?('preview_attachment') &&
-                      options[:download] &&
-                      (attachment.is_image? || attachment.is_pdf? || attachment.is_video? || attachment.is_audio?)
-        attachment_link = super(attachment, options)
-        if previewable
-          filename = attachment.filename
-          bp_src = if attachment.is_image?
-                     'imgSrc'
-                   elsif attachment.is_video?
-                     'vidSrc'
-                   elsif attachment.is_audio?
-                     'audio'
-                   else # attachment.is_pdf?
-                     'iframeSrc'
-                   end
-          url = download_named_attachment_url(attachment, { filename: filename })
-          link_to('',
-                  '#',
-                  :class => 'icon-only icon-zoom-in',
-                  :data => { :bp => filename, :caption => filename, :bp_src => bp_src, :url => url },
-                  :onclick => 'previewAttachment(this)').html_safe + attachment_link
-        else
-          attachment_link
-        end
+        original_link = super(attachment, options.dup)
+        return original_link unless Setting.enabled_redmica_ui_extension_feature?('preview_attachment')
+        return original_link unless options[:class].to_s.include?('icon-download')
+        bp_src = if attachment.is_image?
+                   'imgSrc'
+                 elsif attachment.is_video?
+                   'vidSrc'
+                 elsif attachment.is_audio?
+                   'audio'
+                 elsif attachment.is_pdf?
+                   'iframeSrc'
+                 else
+                   nil
+                 end
+        return original_link unless bp_src
+
+        filename = attachment.filename
+        url = download_named_attachment_url(attachment, { filename: filename })
+        link_to('', '#', :class => 'icon-only icon-zoom-in',
+                :data => { :bp => filename, :bp_src => bp_src, :url => url },
+                :onclick => 'previewAttachment(this)').html_safe + original_link
       end
     end
   end
